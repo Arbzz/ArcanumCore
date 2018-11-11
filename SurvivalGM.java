67a192b8f0770b14b0d1d8f92ac61c44b2b24ce8
Areeb <33463231+Arbzz@users.noreply.github.com>
2018-11-11 21:33:27 +0000

    Create SurvivalGM.java

---
+++ SurvivalGM.java
@@ -0,0 +1,30 @@
+package me.syncnationhd.arc;
+
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+public class SurvivalGM implements CommandExecutor
+{
+  private Main plugin;
+  
+  public SurvivalGM(Main main)
+  {
+    plugin = main;
+  }
+  
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+  {
+    if (((sender instanceof Player)) && (sender.hasPermission("arcanum.survival"))) {
+      Player p = (Player)sender;
+      p.setGameMode(GameMode.SURVIVAL);
+      p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("survival")));
+    } else {
+      sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c[Error] You do not have the required permission to run this command! "));
+    }
+    return false;
+  }
